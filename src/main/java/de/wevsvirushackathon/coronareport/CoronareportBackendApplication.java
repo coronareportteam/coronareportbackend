f140133952875f20eef4cff863f2a6b443a18b20
notableTieView <Mnig,fo1ms>
2020-03-21 15:17:16 +0100

    Enable JPA Repos

--- src/main/java/de/wevsvirushackathon/coronareport/CoronareportBackendApplication.java
+++ src/main/java/de/wevsvirushackathon/coronareport/CoronareportBackendApplication.java
@@ -2,8 +2,10 @@ package de.wevsvirushackathon.coronareport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CoronareportBackendApplication {
 
 	public static void main(String[] args) {
