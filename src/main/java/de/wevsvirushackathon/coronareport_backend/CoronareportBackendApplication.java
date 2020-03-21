5b319664bce67b2c780128185cd2c655d7dbb918
Patrick Otto <patrick-otto23@web.de>
2020-03-21 14:30:14 +0100

    Added initial spring boot application

---
+++ src/main/java/de/wevsvirushackathon/coronareport_backend/CoronareportBackendApplication.java
@@ -0,0 +1,13 @@
+package de.wevsvirushackathon.coronareport_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronareportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronareportBackendApplication.class, args);
+	}
+
+}
