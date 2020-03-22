dc8fb146dde08a20cd0e6566b0159df681048b9d
Patrick Otto <patrick-otto23@web.de>
2020-03-22 23:41:20 +0100

    Anothe camelcase to lowercase fix

--- src/main/java/de/wevsvirushackathon/coronareport/firstReport/FirstReportController.java
+++ src/main/java/de/wevsvirushackathon/coronareport/firstReport/FirstReportController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/firstReport")
+@RequestMapping("/firstreport")
 public class FirstReportController {
 
     private ClientService clientService;
