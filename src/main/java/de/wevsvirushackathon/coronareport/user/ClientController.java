f711118818c9baf5cad43826ce764fa50a6fa652
notableTieView <Mnig,fo1ms>
2020-03-22 12:00:28 +0100

    Add controller for new clients

---
+++ src/main/java/de/wevsvirushackathon/coronareport/user/ClientController.java
@@ -0,0 +1,41 @@
+package de.wevsvirushackathon.coronareport.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/client")
+public class ClientController {
+
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerClient(@RequestBody Client clientDto) {
+        Client client = this.clientService.registerClientAndCreateExternalId(
+                clientDto.getSurename(),
+                clientDto.getFirstname(),
+                clientDto.getPhone(),
+                clientDto.getZipCode(),
+                clientDto.isInfected(),
+                clientDto.getHealthDepartmentId());
+        return ResponseEntity.ok(client.getClientCode());
+    }
+    
+    @GetMapping("/{clientCode}")
+    public ResponseEntity<Client> getClient(@PathVariable String clientCode) {
+        Client client = this.clientService.getClient(clientCode);
+        if (client==null) {
+            // TODO Replace by proper exception handling, not revealing existing client ids
+            throw new IllegalArgumentException("Client not found");
+        }
+        client.setClientId(null);
+        return ResponseEntity.ok(client);
+    }
+
+}
---
+++ src/main/java/de/wevsvirushackathon/coronareport/user/ClientService.java
@@ -0,0 +1,35 @@
+package de.wevsvirushackathon.coronareport.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client registerClientAndCreateExternalId(String surename, String firstname, String phone, String zipCode, boolean infected, String healthDepartmentId) {
+        Client newClient = new Client();
+        newClient.setSurename(surename);
+        newClient.setFirstname(firstname);
+        newClient.setPhone(phone);
+        newClient.setZipCode(zipCode);
+        newClient.setInfected(infected);
+        newClient.setHealthDepartmentId(healthDepartmentId);
+        String clientCode = UUID.randomUUID().toString();
+        newClient.setClientCode(clientCode);
+        this.clientRepository.save(newClient);
+        return newClient;
+    }
+
+    public Client getClient(String clientCode) {
+        return this.clientRepository.findByClientCode(clientCode);
+    }
+}
