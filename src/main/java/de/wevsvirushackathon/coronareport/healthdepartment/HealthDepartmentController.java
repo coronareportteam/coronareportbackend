7059032c3140848ec2986fc525b09b5eff6fdeee
Rudolf Biczok <rcbiczok@gmail.com>
2020-03-30 19:24:54 +0200

    Make use of ModelMapper

--- src/main/java/de/wevsvirushackathon/coronareport/healthdepartment/HealthDepartmentController.java
+++ src/main/java/de/wevsvirushackathon/coronareport/healthdepartment/HealthDepartmentController.java
@@ -1,6 +1,7 @@
 package de.wevsvirushackathon.coronareport.healthdepartment;
 
-import io.swagger.annotations.Api;  
+import io.swagger.annotations.Api;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,21 +18,22 @@ import java.util.stream.StreamSupport;
 public class HealthDepartmentController {
 
     private HealthDepartmentRepository healthDepartmentRepository;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public HealthDepartmentController(HealthDepartmentRepository healthDepartmentRepository) {
+    public HealthDepartmentController(
+            HealthDepartmentRepository healthDepartmentRepository,
+            ModelMapper modelMapper) {
         this.healthDepartmentRepository = healthDepartmentRepository;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
     public ResponseEntity<List<HealthDepartmentDTO>> getHealthDepartments() {
         Iterable<HealthDepartment> all = this.healthDepartmentRepository.findAll();
-        List<HealthDepartmentDTO> dtoList = StreamSupport.stream(all.spliterator(), false).map(h -> {
-            HealthDepartmentDTO healthDepartmentDTO = new HealthDepartmentDTO();
-            healthDepartmentDTO.fullName = h.getFullName();
-            healthDepartmentDTO.id = h.getId();
-            return healthDepartmentDTO;
-        }).collect(Collectors.toList());
+        List<HealthDepartmentDTO> dtoList = StreamSupport
+                .stream(all.spliterator(), false).map(h -> modelMapper.map(h, HealthDepartmentDTO.class))
+                .collect(Collectors.toList());
 
         return ResponseEntity.ok(dtoList);
     }
